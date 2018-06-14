dc07d30f6551e3ff020c3a60ba5acd4b562f1de7
henrikesilverio <henrikesilverio@users.noreply.github.com>
2018-06-13 23:00:24 -0300

    Geração do css, inicialização do site

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,26 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var connect = require('gulp-connect');
+var open = require('gulp-open');
+var serveStatic = require('serve-static');
+
+gulp.task('styles', function () {
+    return gulp.src('src/assets/css/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('src/assets/css'));
+});
+
+gulp.task('host', ['styles'], function () {
+    var settings = {
+        root: 'src',
+        host: 'localhost',
+        port: '8080',
+        fallback: 'src/index.html',
+        middleware: function (connect) {
+            return [connect().use('/node_modules', serveStatic('node_modules'))];
+        }
+    };
+    connect.server(settings);
+    gulp.src('src/index.html')
+        .pipe(open({ uri: 'http://' + settings.host + ':' + settings.port, app: 'chrome' }));
+});
